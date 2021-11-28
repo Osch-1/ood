b49c8c93b9771e1b37ea56897568168f1f70f39e
daniil.gerasimov <daniil.gerasimov@travelline.ru>
2021-11-28 15:05:04 +0300

    Initial commit

---
+++ lw7/lw7/ICanvas.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lw7
+{
+    public interface ICanvas
+    {
+        public void DrawLine( Point from, Point to );
+        public void DrawEllipse( Point leftTop, double width, double height );
+        public void FillEllipse( Point leftTop, double width, double height );
+        public void FillRectangle();
+        public void SetFillColor();
+    }
+}
